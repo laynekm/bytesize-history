3c2067b7a99e66e2e4d95f3c19b500e39fbe5666
LayneKM <laynemikan@gmail.com>
2019-07-13 21:41:09 -0400

    Refactor theme colours so dark theme can be added

--- app/src/main/java/laynekm/bytesize_history/FilterManager.kt
+++ app/src/main/java/laynekm/bytesize_history/FilterManager.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.Filter
 import android.widget.Switch
 
+// TODO: Tidy up filter view, make sure content stays on screen, maybe make text larger
 class FilterManager(var context: Context) {
 
     private val preferencesKey = context.getString(R.string.filter_pref_key)
--- app/src/main/java/laynekm/bytesize_history/NotificationManager.kt
+++ app/src/main/java/laynekm/bytesize_history/NotificationManager.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import java.util.*
 
 // Handles notifications and the shared preferences associated with them
+// TODO: Should notifications abide by the user's preferences? What if they have all eras turned off?
 class NotificationManager(val context: Context) {
 
     private val preferencesKey = context.getString(R.string.notification_pref_key)
