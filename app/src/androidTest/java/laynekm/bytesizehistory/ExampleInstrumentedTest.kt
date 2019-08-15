a5b21c858e049959a3e2ddaaa21db2e67b6fbaba
LayneKM <laynemikan@gmail.com>
2019-08-14 21:00:35 -0400

    Update instrumented test case to reflect new package name

--- app/src/androidTest/java/laynekm/bytesizehistory/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/laynekm/bytesizehistory/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("laynekm.dailyhistory", appContext.packageName)
+        assertEquals("laynekm.bytesizehistory", appContext.packageName)
     }
 }
